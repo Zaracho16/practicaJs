629279d4db235de1bcb63c7953e42f35ff7d1763
Adrian Zaracho <adrianzaracho16@gmail.com>
2026-06-10 18:50:54 -0300

    Nuevos ejercicios

--- dom/script.js
+++ dom/script.js
@@ -26,3 +26,32 @@ obtenerParrafo.style.background = "yellow";
 
 console.log(tituloh1);
 console.log(obtenerParrafo);
+
+/*
+    1) Obtener el botón con getElementById().
+    2) Obtener el h1 con getElementById().
+    3) Agregar un evento click al botón.
+    4) Cuando se haga click, cambiar el texto del h1 a:
+    "Botón presionado"
+    5) Mostrar en consola:
+    "Se hizo click"
+    6) Cambiar el color del h1 a verde cuando se haga click.
+    7) Cambiar el tamaño del h1 a 50px cuando se haga click.
+    8) Cambiar el fondo de la página a lightblue cuando se haga click.
+    9) Cambiar nuevamente el texto a:
+    "Evento ejecutado correctamente"
+    10) Mostrar en consola el elemento botón.
+*/
+
+let obtenerElementoh1 = document.getElementById("tituloMiPagina");
+let obtenerBoton = document.getElementById("botonCambiarTexto");
+
+obtenerBoton.addEventListener("click", () => {
+    obtenerElementoh1.textContent = "Boton presionado";
+    obtenerElementoh1.style.color = "green";
+    obtenerElementoh1.style.fontSize = "50px";
+    console.log("Se hizo click");
+    document.body.style.background = "lightblue";
+    obtenerElementoh1.textContent = "Evento ejecutado correctamente";
+    console.log(obtenerBoton);
+});
